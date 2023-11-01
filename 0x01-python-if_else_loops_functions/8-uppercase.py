4ad799be2c96a944a773e6043aa6e4c2c62b5874
Cobbyheino <danso.cobby@gmail.com>
2023-11-01 00:00:32 +0000

    8uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def islower(c):
+    return ord("a") <= ord(c) <= ord("z")
+
+
+def uppercase(str):
+    for c in str:
+        print("{:c}"
+              .format(ord(c) if not islower(c) else ord(c) - 32),
+              end="")
+    print("")

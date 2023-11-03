98947469f73119099228a49887de9207851fd062
Cobbyheino <danso.cobby@gmail.com>
2023-11-03 06:10:29 +0000

    infinite2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """print args"""
     import sys
-    import math
-    args = sys.argv[1:]
-    result = 0
-    for arg in args:
-        results += int(arg)
 
-    print(result)
+    result = 0
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))

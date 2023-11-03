3e27890ba6965f1fb11b4577ee6e1e67cd2186ce
Cobbyheino <danso.cobby@gmail.com>
2023-11-03 06:04:20 +0000

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import math
-    totalsum = 0
-    for i in sys.argv:
-        totalsum += int(i)
-        print("{}".format(totalsum))
+    args = sys.argv[1:]
+    result = 0
+    for arg in args:
+        results += int(arg)
+
+    print(result)
